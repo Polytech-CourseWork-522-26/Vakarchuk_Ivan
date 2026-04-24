4bbc33e2a1b9acc906a414202423c3c78080714d
Zxc <ivanvakr@gmail.com>
2026-04-24 11:43:12 +0300

    Update RecipeData structure with new fields
    Added new fields for author_id, type, and tags. Increased size of ingredients and instructions.

--- RecipeStruct.h
+++ RecipeStruct.h
@@ -3,12 +3,15 @@
 
 struct RecipeData {
     int id;
+    int author_id;
     char author[50];
-    char name[100];         
+    char name[100];
     char description[255];
-    char ingredients[500];  
-    char instructions[100000]; 
+    char ingredients[1000];
+    char instructions[5000];
     int calories;
+    char type[50];
+    char tags[100];
 };
 
 #endif
